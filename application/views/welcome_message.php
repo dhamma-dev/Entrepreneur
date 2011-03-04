957e71c5581b3c67971bc75bd34ae19ee71af635
dev <xiao.ma.hall@gmail.com>
2011-03-04 10:18:47 -0800

    Changed welcome message H1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -45,7 +45,7 @@ code {
 </head>
 <body>
 
-<h1>Welcome to CodeIgniter!</h1>
+<h1>Hello Team, Let's rock this project!</h1>
 
 <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
